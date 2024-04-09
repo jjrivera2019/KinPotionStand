332845d2415dc0a464368cb39de038b5d158cad1
Joaquin Rivera <jjrivera2019@gmail.com>
2024-04-08 23:34:37 -0700

    2 more mins

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -12,8 +12,11 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    with db.engine.begin() as connection:
+        currGreenPots = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
+        currGreenML = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar()
+        currGold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
+    return {"number_of_potions": currGreenPots, "ml_in_barrels": currGreenML, "gold": currGold}
 
 # Gets called once a day
 @router.post("/plan")
